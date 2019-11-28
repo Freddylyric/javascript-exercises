fe5f60166fb0c18cd5852885f13e671fd5e27673
Freddy Lyric <freddykk20@gmail.com>
2019-11-28 21:04:28 +0300

    nine

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -219,7 +219,7 @@ getContext = getContext.bind(differentContext);
                 // Unfortunately, it does not specify any parameters.
                 // Find a way to retrieve the first argument without specifying a parameter.
                 // ✏️ ADD CODE HERE ✏️
-
+return arguments[0];
                 ////////////////////////////////////////////////////////////////////
             }
 
