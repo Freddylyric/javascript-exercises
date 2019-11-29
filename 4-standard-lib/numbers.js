797e6a28e3e0ba6680f8c3e2b9055970336b5de8
Freddy Lyric <freddykk20@gmail.com>
2019-11-29 22:57:16 +0300

    test six

--- 4-standard-lib/numbers.js
+++ 4-standard-lib/numbers.js
@@ -105,7 +105,9 @@ function isNotANumber(number){
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
-
+            function getSmallestNumber(number){
+                return Math.min(...number);
+            }
             ////////////////////////////////////////////////////////////////////////
 
             expect(getSmallestNumber([-1])).to.equal(-1);
